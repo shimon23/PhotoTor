3fb50724e9671cc04787ffafe06e76755892e0d0
Netanel Hugi <netanelhugi@gmail.com>
2019-12-16 10:06:53 +0200

    update gradle file

--- app/src/main/java/com/example/phototor/MainActivity.java
+++ app/src/main/java/com/example/phototor/MainActivity.java
@@ -4,11 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import android.content.Intent;
+
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null)
+        {
+            startActivity(new Intent(this, signInUp.class));
+            finish();
+        }
+        else
+        {
+            authenticateUser();
+        }
+
     }
+
+    private void authenticateUser(){
+
+//        List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+//        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+//                .setAvailableProviders(providers)
+//                .build(), REQUEST_CODE);
+        int i = 5;
+    }
+
+
+
 }
