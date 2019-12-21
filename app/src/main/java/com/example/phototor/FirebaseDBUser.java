397738cdcfb40532023d61dae1ebdb8008fc3bf0
Netanel Hugi <netanelhugi@gmail.com>
2019-12-21 21:38:01 +0200

    fixing user data saving on register

--- app/src/main/java/com/example/phototor/FirebaseDBUser.java
+++ app/src/main/java/com/example/phototor/FirebaseDBUser.java
@@ -1,34 +1,49 @@
 package com.example.phototor;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.widget.Toast;
+
 import java.util.HashMap;
 import java.util.Map;
+
+import android.content.Context;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 import com.google.firebase.database.DatabaseReference;
 
 public class FirebaseDBUser extends FirbaseModel {
 
-    public void addUserToDB(String id,String firstName, String lastName, String email, String phoneNumber, String city,String userType){
+    public ProgressDialog progressDialog;
 
-        if(userType=="client"){
+
+    public void addUserToDB(String id, String firstName, String lastName, String email, String phoneNumber, String city, String userType) {
+
+        if (userType == "client") {
             return;
-        }
-        else{
+        } else {
             writeNewPhotographer(id, firstName, lastName, email, phoneNumber, city);
 
         }
 
     }
 
-    public void writeNewPhotographer(String id,String firstName, String lastName, String email, String phoneNumber, String city){
+    public void writeNewPhotographer(String id, String firstName, String lastName, String email, String phoneNumber, String city) {
 
         final Map<String, Object> dataMap = new HashMap<String, Object>();
-        photographer userPhoto = new photographer(id,firstName,lastName,email,phoneNumber,city);
+        photographer userPhoto = new photographer(id, firstName, lastName, email, phoneNumber, city);
         dataMap.put("user details", userPhoto.toMap());
         dbRef.child("users").child("photographers").child(id).updateChildren(dataMap);
 
+
     }
 
-    public DatabaseReference getUserFromDB(String userId){
+    public DatabaseReference getUserFromDB(String userId) {
         return dbRef.getRef().child("photographers").child(userId);
     }
 
