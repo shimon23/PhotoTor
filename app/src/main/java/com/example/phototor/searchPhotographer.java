5391fc3a7f1f281f98f70c9e048773e7dbbee60d
Netanel Hugi <netanelhugi@gmail.com>
2020-01-11 21:30:35 +0200

    update photographer search

--- app/src/main/java/com/example/phototor/searchPhotographer.java
+++ app/src/main/java/com/example/phototor/searchPhotographer.java
@@ -1,5 +1,7 @@
 package com.example.phototor;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,11 +9,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +27,7 @@ public class searchPhotographer extends AppCompatActivity implements View.OnClic
     Button send;
     DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
     EditText userName;
+    Spinner areas;
 
 
     @Override
@@ -27,32 +35,97 @@ public class searchPhotographer extends AppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_photographer);
 
-        send = (Button) findViewById(R.id.sendBtn);
+        send = (Button) findViewById(R.id.searchBtn);
         userName = (EditText) findViewById(R.id.userText);
+        areas = (Spinner) findViewById(R.id.areasSppiner);
     }
 
     @Override
     public void onClick(View view) {
 
         if(view == send){
-            Log.d("SEARCH", userName.getText().toString());
 
-            final Map<String, Object> dataMap = new HashMap<String, Object>();
-            dataMap.put("netanel", "hugi");
-
-            dbRef.child("orders").updateChildren(dataMap).addOnSuccessListener(new OnSuccessListener<Void>() {
-                @Override
-                public void onSuccess(Void aVoid) {
-//                    progressDialog.dismiss();
-//                    next.setClickable(true);
-//                    next.setVisibility(View.VISIBLE);
-                }
-            });
+            String area = areas.getSelectedItem().toString();
+            Log.d("SEARCH!", area);
+            findTest("areas",area);
+//
+//            final Map<String, Object> dataMap = new HashMap<String, Object>();
+//            dataMap.put("netanel", "hugi");
+//
+//            dbRef.child("orders").updateChildren(dataMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+//                @Override
+//                public void onSuccess(Void aVoid) {
+////                    progressDialog.dismiss();
+////                    next.setClickable(true);
+////                    next.setVisibility(View.VISIBLE);
+//                }
+//            });
 
 
         }
 
 
+
+    }
+
+
+    public void findTest(final String dbName, final String op){
+
+        dbRef = FirebaseDatabase.getInstance().getReference(dbName);
+
+        dbRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+
+                try {
+
+                    Object fieldsObj = new Object();
+                    ArrayList<HashMap<String, Object>> list = new ArrayList<>();
+                    HashMap fldObj = (HashMap)dataSnapshot.getValue(fieldsObj.getClass());
+
+                    Log.d("findTest", fldObj.toString());
+
+//
+//                    fldObj.put("recKeyID", dataSnapshot.getKey());
+//                    String userType = fldObj.get(op).toString();
+//                    list.add(fldObj);
+//
+//                    Log.d("USER-Hash", fldObj.toString());
+//                    String value= dataSnapshot.getValue(User.class).toString();
+//
+//                    if(userType.equals("photographer")) {
+//                        String userId = fldObj.get("recKeyID").toString();
+//                    }
+                }
+                catch (Exception ex){
+
+                }
+
+
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
